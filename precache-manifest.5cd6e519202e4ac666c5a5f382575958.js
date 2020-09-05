f5bf79b531ee84835030dea059e7eedd21047bc0
deeAjith <41795417+DeeAjith@users.noreply.github.com>
2020-09-06 01:04:31 +0530

    Updates

---
+++ precache-manifest.5cd6e519202e4ac666c5a5f382575958.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22f340f2771130d869b56b336c8744a",
+    "url": "/react_day_1/index.html"
+  },
+  {
+    "revision": "20ff9ed89cf799e0cbb0",
+    "url": "/react_day_1/static/css/main.7594cc0d.chunk.css"
+  },
+  {
+    "revision": "d56ad9cd1a3132cc5a06",
+    "url": "/react_day_1/static/js/2.e441d054.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_day_1/static/js/2.e441d054.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20ff9ed89cf799e0cbb0",
+    "url": "/react_day_1/static/js/main.486bc6a4.chunk.js"
+  },
+  {
+    "revision": "1f30085a3e912b60ae83",
+    "url": "/react_day_1/static/js/runtime-main.102f1e52.js"
+  }
+]);
